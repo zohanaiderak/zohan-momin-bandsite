4779281883e9327b6fec72311fecfc1867fc86d4
Zohan Momin <zohanaiderak@gmail.com>
2020-05-02 17:19:17 -0400

    updated js for shows page

--- Sprint-2/JavaScript/shows.js
+++ Sprint-2/JavaScript/shows.js
@@ -53,29 +53,48 @@ let divwrap = document.createElement("div");
 divwrap.setAttribute('class','mainDiv');
 let sec = document.querySelector(".shows");
 sec.appendChild(divwrap);
-let divrow = document.createElement("div");
-divrow.setAttribute('class', "row");
-divwrap.appendChild(divrow);
-function displayComment(el1 , el2){
+
+    
+function displayComment(el1 , el2 , el3, el4 , el5, el6){
+    let divrow = document.createElement("div");
+    divrow.setAttribute('class', "row");
+    divwrap.appendChild(divrow);
     let divcol = document.createElement("div");
     divcol.setAttribute('class', "row__column");
     divrow.appendChild(divcol);
     let divstyl = document.createElement("div");
     divstyl.setAttribute('class', "row__column--styling");
-    divcol.appendChild(divstyl);
-
+    let divstyl2 = document.createElement("div");
+    divstyl2.setAttribute('class', "row__column--styling2");
+    let divstyl3 = document.createElement("div");
+    divstyl3.setAttribute('class', "row__column--styling3");
     let pel1 = document.createElement('p');
     let pel2 = document.createElement('p');
+    let pel3 = document.createElement('p');
+    let pel4 = document.createElement('p');
+    let pel5 = document.createElement('p');
+    let pel6 = document.createElement('p');
 //     let pComm = document.createElement('p');
 //     ul.appendChild(li);
        divstyl.appendChild(pel1);
        pel1.innerHTML = pel1.innerHTML + el1;
        divstyl.appendChild(pel2);
        pel2.innerHTML = pel2.innerHTML + el2;
+       divstyl2.appendChild(pel3);
+       pel3.innerHTML = pel3.innerHTML + el3;
+       divstyl2.appendChild(pel4);
+       pel4.innerHTML = pel4.innerHTML + el4;
+       divstyl3.appendChild(pel5);
+       pel5.innerHTML = pel5.innerHTML + el5;
+       divstyl3.appendChild(pel6);
+       pel6.innerHTML = pel6.innerHTML + el6;
 //     li.appendChild(pTime);
 //     pComm.innerHTML = pComm.innerHTML + comment;
 //     li.appendChild(pComm);
        divcol.innerHTML = divcol.innerHTML
+       divcol.appendChild(divstyl);
+       divcol.appendChild(divstyl2);
+       divcol.appendChild(divstyl3);
 //     li.innerHTML = li.innerHTML ;
 //     let ulComm = document.querySelector(".post");
 //     let allLi = ulComm.querySelectorAll("li");
@@ -99,9 +118,8 @@ function displayComment(el1 , el2){
 }
 
 for(var i = 0;i<showTime.length;i++){
-    displayComment(showTime[i].date ,showTime[i].datetext);
-    displayComment(showTime[i].venue ,showTime[i].venueadd);
-    displayComment(showTime[i].Location ,showTime[i].Locationadd);
+    displayComment(showTime[i].date ,showTime[i].datetext ,showTime[i].venue ,showTime[i].venueadd ,showTime[i].Location ,showTime[i].Locationadd);
+
 }
 
 // for(var i=0 ; i<comments.length ; i++ ){

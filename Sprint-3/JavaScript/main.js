4766dba1b89b6822125d7d6441883f5ae2b4856b
Zohan Momin <zohanaiderak@gmail.com>
2020-05-07 19:54:10 -0400

    Worked on improvements

--- Sprint-3/JavaScript/main.js
+++ Sprint-3/JavaScript/main.js
@@ -23,18 +23,8 @@ document.getElementById('section').appendChild(divUl);
 Commenting = (res) =>{
     comments =  res.data;
     console.log(comments);
-    let btn = document.querySelector('.comments');
-    btn.addEventListener('submit', e =>{
-    e.preventDefault();
-    document.getElementById('div__form').innerHTML = "";   //to clear the comments section upon click and then the code runs and re enters all comments.
-    let newcomment={};
-    let text = document.getElementById('name');
-    let textArea = document.getElementById('textArea');
-    newcomment.name = text.value;
-    newcomment.timestamp = datetime ;
-    newcomment.comment = textArea.value;
-    comments.push(newcomment);
-    function displayComment(name , timestamp, comment){
+
+    let displayComment = (object) =>{
         let ul = document.createElement("ul");
         ul.setAttribute('class', "post");
         divUl.prepend(ul);
@@ -44,18 +34,16 @@ Commenting = (res) =>{
         let pComm = document.createElement('p');
         ul.appendChild(li);
         li.appendChild(pName);
-        pName.innerHTML = pName.innerHTML + name;
+        pName.innerHTML = pName.innerHTML + object.name;
         li.appendChild(pTime);
-        pTime.innerHTML = pTime.innerHTML + timestamp;
+        pTime.innerHTML = pTime.innerHTML + object.timestamp;
         li.appendChild(pTime);
-        pComm.innerHTML = pComm.innerHTML + comment;
+        pComm.innerHTML = pComm.innerHTML + object.comment;
         li.appendChild(pComm);
         li.innerHTML = li.innerHTML ;
         let ulComm = document.querySelector(".post");
         let allLi = ulComm.querySelectorAll("li");
         let l= allLi.length;
-        let clrinput = document.querySelector(".comments");
-        clrinput.reset();
         for (var i =0 ; i < l; i++){
             allLi[i].classList.add('post__comment');
         }
@@ -72,26 +60,12 @@ Commenting = (res) =>{
             allp[i].classList.add('post__comment--name');
             allp[i].classList.add('timestamp');
         }
-        // setInterval((e) => {
-        //     let ptime = document.querySelectorAll('.timestamp');
-        //     timestamp = timestamp + 1 ;
-        //     for(i=0;i<comments.length;i++){
-        //         if(timestamp<59){
-        //             ptime[i].innerHTML = " ";
-        //             ptime[i].innerHTML = timestamp + ' m ago';
-        //         }
-        //         if(timestamp>59){
-        //             ptime[i].innerHTML = " ";
-        //             ptime[i].innerHTML = timestamp + ' h ago';
-        //         }
-        //         ;
-        //     }
-        // }, 60000);   
     }
     
     for(var i = 0;i<comments.length;i++){
-        displayComment(comments[i].name ,comments[i].timestamp ,comments[i].comment );
-    } 
+        displayComment(comments[i]);
+    }
+    
     for(var i=0 ; i<comments.length ; i++ ){
         let ulsel = document.querySelectorAll('.post');
         let secli = document.createElement('li');
@@ -99,76 +73,36 @@ Commenting = (res) =>{
         let faceimg = document.createElement('img');
         faceimg.setAttribute('class', 'post__dp-grey');
         secli.appendChild(faceimg);
-        ulsel[i].prepend(secli);
-        }
-    
-    }
-)
-
-    
-function displayComment(name , timestamp, comment){
-    let ul = document.createElement("ul");
-    ul.setAttribute('class', "post");
-    divUl.prepend(ul);
-    let li = document.createElement('li');
-    let pName = document.createElement('p');
-    let pTime = document.createElement('p');
-    let pComm = document.createElement('p');
-    ul.appendChild(li);
-    li.appendChild(pName);
-    pName.innerHTML = pName.innerHTML + name;
-    li.appendChild(pTime);
-    pTime.innerHTML = pTime.innerHTML + timestamp;
-    li.appendChild(pTime);
-    pComm.innerHTML = pComm.innerHTML + comment;
-    li.appendChild(pComm);
-    li.innerHTML = li.innerHTML ;
-    let ulComm = document.querySelector(".post");
-    let allLi = ulComm.querySelectorAll("li");
-    let l= allLi.length;
-    for (var i =0 ; i < l; i++){
-        allLi[i].classList.add('post__comment');
-    }
-    let divf = document.querySelector('.div__form')
-    let allp = divf.querySelectorAll("p");
-    let le= allp.length;
-    for (var i =0 ; i < le-1 ; i=i+3){
-        allp[i].classList.add('post__comment--name');
-    }
-    for (var i =le-1 ; i >= 0; i=i-3){
-        allp[i].classList.add('post__comment--text');
-    }
-    for (var i =le-2 ; i >= 0; i=i-3){
-        allp[i].classList.add('post__comment--name');
-        allp[i].classList.add('timestamp');
+        ulsel[i].prepend(secli); 
     }
     
-    // setInterval((e) => {
-    //     let ptime = document.querySelectorAll('.timestamp');
-    //     timestamp = timestamp + 1 ;
-    //     for(i=0;i<comments.length;i++){
-    //         if(timestamp<59){
-    //             ptime[i].innerHTML = " ";
-    //             ptime[i].innerHTML = timestamp + ' m ago';
-    //         }
-    //     }
-    // }, 60000);
+    let btn = document.querySelector('.comments');
+    btn.addEventListener('submit', e =>{
+        e.preventDefault();
+        document.getElementById('div__form').innerHTML = "";   //to clear the comments section upon click and then the code runs and re enters all comments.
+        let newcomment={};
+        let text = document.getElementById('name');
+        let textArea = document.getElementById('textArea');
+        newcomment.name = text.value;
+        newcomment.timestamp = datetime ;
+        newcomment.comment = textArea.value;
+        comments.push(newcomment);
+    
+        for(var i = 0;i<comments.length;i++){
+            displayComment(comments[i]);
+        } 
+        for(var i=0 ; i<comments.length ; i++ ){
+            let ulsel = document.querySelectorAll('.post');
+            let secli = document.createElement('li');
+            secli.setAttribute('class', 'post__dp')
+            let faceimg = document.createElement('img');
+            faceimg.setAttribute('class', 'post__dp-grey');
+            secli.appendChild(faceimg);
+            ulsel[i].prepend(secli);
+        }    
+    })
 }
 
-for(var i = 0;i<comments.length;i++){
-    displayComment(comments[i].name ,comments[i].timestamp ,comments[i].comment );
-}
-
-for(var i=0 ; i<comments.length ; i++ ){
-    let ulsel = document.querySelectorAll('.post');
-    let secli = document.createElement('li');
-    secli.setAttribute('class', 'post__dp')
-    let faceimg = document.createElement('img');
-    faceimg.setAttribute('class', 'post__dp-grey');
-    secli.appendChild(faceimg);
-    ulsel[i].prepend(secli); 
-}
-}
 
 
 
--- Sprint-3/JavaScript/shows.js
+++ Sprint-3/JavaScript/shows.js
@@ -1,106 +1,111 @@
 let commentsAPIKey = "7395cb0b-0998-4427-adcc-f980b769873e";
+let getshows = () =>{ 
+            axios
+            .get(`https://project-1-api.herokuapp.com/showdates?api_key=${commentsAPIKey}`)
+            .then((results) =>{
+                showTime(results);
+            })
+            .catch((err) => ('error',err));
+}
+getshows();
+
 let divwrap = document.createElement("div");
 divwrap.setAttribute('class','mainDiv');
 let sec = document.querySelector(".shows");
 sec.appendChild(divwrap);
 
-let promise = axios.get(`https://project-1-api.herokuapp.com/showdates?api_key=${commentsAPIKey}`);
-promise.then(res =>{
-    let showdates =  res.data;
-    console.log(showdates);   
-function displayComment(el2 , el4 ,el6){
-    let divrow = document.createElement("div");
-    divrow.setAttribute('class', "row");
-    divwrap.appendChild(divrow);
-    let divcol = document.createElement("div");
-    divcol.setAttribute('class', "row__column");
-    divrow.appendChild(divcol);
-    let divstyl = document.createElement("div");
-    divstyl.setAttribute('class', "row__column--styling");
-    let divstyl2 = document.createElement("div");
-    divstyl2.setAttribute('class', "row__column--styling2");
-    let divstyl3 = document.createElement("div");
-    divstyl3.setAttribute('class', "row__column--styling3");
-    let pel1 = document.createElement('p');
-    pel1.setAttribute('class', 'row-head');
-    let pel2 = document.createElement('p');
-    pel2.setAttribute('class', 'row-child');
-    let pel3 = document.createElement('p');
-    pel3.setAttribute('class', 'row-head');
-    let pel4 = document.createElement('p');
-    pel4.setAttribute('class', 'row-child');
-    let pel5 = document.createElement('p');
-    pel5.setAttribute('class', 'row-head');
-    let pel6 = document.createElement('p');
-    pel6.setAttribute('class', 'row-child');
-    divstyl.appendChild(pel1);
-    pel1.innerHTML = pel1.innerHTML + "DATE";
-    divstyl2.appendChild(pel3);
-    pel2.innerHTML = pel2.innerHTML + el2;
-    divstyl2.appendChild(pel4);
-    pel3.innerHTML = pel3.innerHTML + "VENUE";
-    divstyl3.appendChild(pel5);
-    pel4.innerHTML = pel4.innerHTML + el4;
-    divstyl3.appendChild(pel6);
-    pel5.innerHTML = pel5.innerHTML + "LOCATION";
-    divstyl.appendChild(pel2);
-    pel6.innerHTML = pel6.innerHTML + el6;
-    divcol.innerHTML = divcol.innerHTML+""
-    divcol.appendChild(divstyl);
-    divcol.appendChild(divstyl2);
-    divcol.appendChild(divstyl3);
+showTime = (res) =>{
+    showdates =  res.data;
+    console.log(showdates);
+    function displayShows(object){
+        let divrow = document.createElement("div");
+        divrow.setAttribute('class', "row");
+        divwrap.appendChild(divrow);
+        let divcol = document.createElement("div");
+        divcol.setAttribute('class', "row__column");
+        divrow.appendChild(divcol);
+        let divstyl = document.createElement("div");
+        divstyl.setAttribute('class', "row__column--styling");
+        let divstyl2 = document.createElement("div");
+        divstyl2.setAttribute('class', "row__column--styling2");
+        let divstyl3 = document.createElement("div");
+        divstyl3.setAttribute('class', "row__column--styling3");
+        let pel1 = document.createElement('p');
+        pel1.setAttribute('class', 'row-head');
+        let pel2 = document.createElement('p');
+        pel2.setAttribute('class', 'row-child');
+        let pel3 = document.createElement('p');
+        pel3.setAttribute('class', 'row-head');
+        let pel4 = document.createElement('p');
+        pel4.setAttribute('class', 'row-child');
+        let pel5 = document.createElement('p');
+        pel5.setAttribute('class', 'row-head');
+        let pel6 = document.createElement('p');
+        pel6.setAttribute('class', 'row-child');
+        divstyl.appendChild(pel1);
+        pel1.innerText = pel1.innerText + "DATE";
+        divstyl2.appendChild(pel3);
+        pel2.innerText = pel2.innerText + object.date;
+        divstyl2.appendChild(pel4);
+        pel3.innerText = pel3.innerText + "VENUE";
+        divstyl3.appendChild(pel5);
+        pel4.innerText = pel4.innerText + object.place;
+        divstyl3.appendChild(pel6);
+        pel5.innerText = pel5.innerText + "LOCATION";
+        divstyl.appendChild(pel2);
+        pel6.innerText = pel6.innerText + object.location;
+        divcol.innerText = divcol.innerText + ""
+        divcol.appendChild(divstyl);
+        divcol.appendChild(divstyl2);
+        divcol.appendChild(divstyl3);
+    }
+
+    for(var i = 0;i<showdates.length;i++){
+        displayShows(showdates[i]);
+    }
+    for(var i=0 ; i<showdates.length; i++ ){
+        let divparent = document.querySelectorAll('.row__column');
+        let divbtn = document.createElement('div');
+        divbtn.setAttribute('class', 'row__column--styling4')
+        let btn = document.createElement('button');
+        btn.setAttribute('class', 'shows-button');
+        btn.innerText= 'BUY TICKETS'
+        divbtn.appendChild(btn);
+        divparent[i].appendChild(divbtn);
+    }
+    for(var i=0;i<showdates.length;i++){
+        let sechd = document.querySelectorAll(".row");
+        let rowhd = sechd[i].querySelector('.row-child');
+        rowhd.classList.add('row-child--date');
+    }
+    
+    let mainhd = document.createElement('div');
+    mainhd.setAttribute('class', 'row');
+    mainhd.classList.add('row__headShow');
+    let colhd = document.createElement('div');
+    colhd.setAttribute('class', 'row__column');
+    colhd.classList.add('col-height');
+    let hd1 = document.createElement('p');
+    hd1.setAttribute('class' , 'row__column--styling');
+    hd1.classList.add('row__head');
+    hd1.innerText = "DATE";
+    colhd.appendChild(hd1);
+    let hd2 = document.createElement('p');
+    hd2.setAttribute('class' , 'row__column--styling2');
+    hd2.classList.add('row__head');
+    hd2.innerText = "VENUE";
+    colhd.appendChild(hd2);
+    let hd3 = document.createElement('p');
+    hd3.setAttribute('class' , 'row__column--styling3');
+    hd3.classList.add('row__head');
+    hd3.innerText = "LOCATION";
+    colhd.appendChild(hd3);
+    let hd4 = document.createElement('button');
+    hd4.setAttribute('class' , 'btn-hidden');
+    mainhd.classList.add('row__headShow');
+    hd4.innerText = "         ";
+    colhd.appendChild(hd4);
+    let maindv = document.querySelector('.mainDiv');
+    mainhd.appendChild(colhd);
+    maindv.prepend(mainhd);
 }
-
-for(var i = 0;i<showdates.length;i++){
-    displayComment(showdates[i].date ,showdates[i].place ,showdates[i].location);
-}
-
-for(var i=1 ; i<showdates.length+1 ; i++ ){
-    let divparent = document.querySelectorAll('.row__column');
-    let divbtn = document.createElement('div');
-    divbtn.setAttribute('class', 'row__column--styling4')
-    let btn = document.createElement('button');
-    btn.setAttribute('class', 'shows-button');
-    btn.innerHTML= 'BUY TICKETS'
-    divbtn.appendChild(btn);
-    divparent[i].appendChild(divbtn);
-}
-
-for(var i=0;i<showdates.length;i++){
-    let sechd = document.querySelectorAll(".row");
-    let rowhd = sechd[i].querySelector('.row-child');
-    rowhd.classList.add('row-child--date');
-}
-});
-promise.catch(err => console.log('error',err));
-
-let mainhd = document.createElement('div');
-mainhd.setAttribute('class', 'row');
-mainhd.classList.add('row__headShow');
-let colhd = document.createElement('div');
-colhd.setAttribute('class', 'row__column');
-colhd.classList.add('col-height');
-let hd1 = document.createElement('p');
-hd1.setAttribute('class' , 'row__column--styling');
-hd1.classList.add('row__head');
-hd1.innerHTML = "DATE";
-colhd.appendChild(hd1);
-let hd2 = document.createElement('p');
-hd2.setAttribute('class' , 'row__column--styling2');
-hd2.classList.add('row__head');
-hd2.innerHTML = "VENUE";
-colhd.appendChild(hd2);
-let hd3 = document.createElement('p');
-hd3.setAttribute('class' , 'row__column--styling3');
-hd3.classList.add('row__head');
-hd3.innerHTML = "LOCATION";
-colhd.appendChild(hd3);
-let hd4 = document.createElement('button');
-hd4.setAttribute('class' , 'btn-hidden');
-mainhd.classList.add('row__headShow');
-hd4.innerHTML = "         ";
-colhd.appendChild(hd4);
-let maindv = document.querySelector('.mainDiv');
-mainhd.appendChild(colhd);
-maindv.prepend(mainhd);
-
